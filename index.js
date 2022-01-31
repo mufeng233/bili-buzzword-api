695bbab000aa1c00f3c6ed0c2d4e746f65e915e8
mufeng2333 <2441260435@qq.com>
2022-01-31 11:28:02 +0800

    update

--- index.js
+++ index.js
@@ -1,8 +1,12 @@
 const express = require("express");
 const app = express();
+const product = require("./api/product");
 const buzzword = require("./api/buzzword");
 
-app.use("/api/buzzword", buzzword);
+app.use(express.json({ extended: false }));
+
+app.use("/product", product);
+app.use("/buzzword", buzzword);
 
 const PORT = process.env.PORT || 8080;
 app.listen(PORT, () => console.log(`Server is running in port ${PORT}`));
