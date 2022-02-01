1dd857176b3813e56f3c8e3d05293280069dfd3f
mufeng2333 <2441260435@qq.com>
2022-02-01 21:54:41 +0800

    update

--- index.js
+++ index.js
@@ -3,6 +3,14 @@ const app = express();
 const product = require("./api/product");
 const buzzword = require("./api/buzzword");
 
+app.all("*", function (req, res, next) {
+  res.header("Access-Control-Allow-Origin", "*");
+  res.header("Access-Control-Allow-Headers", "Content-Type");
+  res.header("Access-Control-Allow-Methods", "*");
+  res.header("Content-Type", "application/json;charset=utf-8");
+  next();
+});
+
 app.use(express.json({ extended: false }));
 
 app.use("/product", product);
